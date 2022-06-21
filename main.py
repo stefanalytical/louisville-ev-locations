afdea719e8c99d5748fa3d467c24dbdc1de82aab
Stefan Vuleta <vuleta99@gmail.com>
2022-06-21 02:41:10 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
 df = pd.read_csv (r'/Users/stefanvuleta/louisville_ev/louisville_ev.csv')
+
+df.drop(['coun_dist'], axis = 1, inplace = True)
 print(df)
+
+col = "num_votes"
+max_x = df.loc[df[col].idxmax()]
+print("Maximum value of column ", col, " is:\n", max_x)
+
