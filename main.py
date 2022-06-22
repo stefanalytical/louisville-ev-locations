24253ed59fea85e845799cb8f38b3220c2627f31
Stefan Vuleta <vuleta99@gmail.com>
2022-06-21 23:42:48 -0400

    Dropped and renamed columns

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import pandas as pd
 
 df = pd.read_csv (r'/Users/stefanvuleta/louisville_ev/louisville_evs.csv')
 print(df)
+
+df.drop(['OBJECTID', 'FID_EV_Charging_Suggestions', 'Comments', 'Location', 'Date', 'CreationDate', 'COUNDIST'], axis = 1, inplace = True)
+
+print(df.columns)
+
+fixed_columns = {
+    'NUMVOTES':'Num_Votes',
+    'ZIPCODE':'Zip_Code',
+
+}
+
+df.rename(columns = fixed_columns, inplace = True)
+print(df)
