8361bd61126afcb2dca32782479d54dd3309cfe7
Stefan Vuleta <vuleta99@gmail.com>
2022-06-22 00:06:12 -0400

    Dropped rows with value zero

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ df = pd.read_csv (r'/Users/stefanvuleta/louisville_ev/louisville_evs.csv')
 print(df)
 
 df.drop(['OBJECTID', 'FID_EV_Charging_Suggestions', 'Comments', 'Location', 'Date', 'CreationDate', 'COUNDIST'], axis = 1, inplace = True)
-
-print(df.columns)
+df = df.dropna()
 
 fixed_columns = {
     'NUMVOTES':'Num_Votes',
     'ZIPCODE':'Zip_Code',
-
 }
 
 df.rename(columns = fixed_columns, inplace = True)
+
+df = df[df.Num_Votes != 0]
 print(df)
