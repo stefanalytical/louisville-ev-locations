34404911952ea3671cb96bd288a1a80659bb1ba8
Stefan Vuleta <vuleta99@gmail.com>
2022-06-24 02:22:02 -0400

    Scrape data from html table

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv ('louisville_evs.csv')
-print(df)
-
 df.drop(['OBJECTID', 'FID_EV_Charging_Suggestions', 'Comments', 'Location', 'Date', 'CreationDate', 'COUNDIST'], axis = 1, inplace = True)
 df = df.dropna()
 
@@ -15,3 +13,31 @@ df.rename(columns = fixed_columns, inplace = True)
 
 df = df[df.Num_Votes != 0]
 print(df)
+
+# Scrape data from html table
+url = 'https://localistica.com/usa/ky/louisville/zipcodes/highest-household-income-zipcodes/'
+scraper = pd.read_html(url)
+
+# Get first table                                                                                                           
+df2 = scraper[0]
+
+# Drop and rename columns
+df2.drop([1, 3], axis = 1, inplace = True)
+fixed_columns = {
+    0:'Zip_Code',
+    2:'Population',
+    4:'Age',
+    5:'AVG_Income',
+
+}
+
+df2.rename(columns = fixed_columns, inplace = True)
+
+# Drop first row with incorrect headings
+df2 = df2.drop(df2.index[0])
+print(df2)
+
+
+
+
+
