cf368da4a3935981047bc2c8a2dbc6d6e6c62db6
Stefan Vuleta <vuleta99@gmail.com>
2022-06-26 01:11:35 -0400

    Created new calculated column

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv ('clean_data/new_csv/new_louisville_evs.csv')
+df = pd.read_csv ('clean_data/cleaned_louisville_evs.csv')
+
+df['Zip_Total_Income'] = df.Population_Per_Zip * df.Income_Per_Zip
+
+df = df.astype({"Zip_Total_Income": int})
+df = df.astype({"Income_Per_Zip": int})
+
 print(df)
