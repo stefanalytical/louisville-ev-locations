df0191b41a887d9c6d6cdbb14cad8129ad088245
Stefan Vuleta <vuleta99@gmail.com>
2022-07-25 18:07:53 -0400

    Changed calculated ratio column

--- data_cleanup.py
+++ data_cleanup.py
@@ -117,7 +117,7 @@ final_df = final_df.drop_duplicates()
 final_df = final_df.sort_values('Num_Votes', ascending=False)
 
 # Create new calculated ratio column and set decimal places to 5
-final_df['Theft_Pop_Ratio'] = final_df['Vehicle_Theft_Per_Zip'] / final_df['Zip_Population']
+final_df['Theft_Pop_Ratio'] = final_df['Vehicle_Theft_Per_Zip'] / final_df['Num_Votes']
 final_df['Theft_Pop_Ratio'] = final_df['Theft_Pop_Ratio'].round(decimals = 5)
 
 # Define function filter to categorize int data to str
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv('clean_data/cleaned_louisville_evs.csv')
-print(df)
+
+table = pd.pivot_table(df, index=['Zip_Pop_Size', 'Income_Group'], 
+                       values=['Num_Votes'], aggfunc={'Num_Votes': 'count',})
+
+table2 = pd.pivot_table(df, index=['Vehicle_Theft_Likeliness', 'Income_Group'], 
+                       values=['Num_Votes'], aggfunc={'Num_Votes': 'count',})
+
+print(table)
+print(table2)
